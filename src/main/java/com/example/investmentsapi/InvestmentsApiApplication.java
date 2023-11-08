de2adb16d535b2cb16290389d751667cca941f34
thando <tzulu49@gmail.com>
2023-11-08 03:47:52 +0200

    Initial Commit

---
+++ src/main/java/com/example/investmentsapi/InvestmentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.investmentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/investmentsapi/InvestmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investmentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
