35a2075ff6252df12bc5aa967ddf29c78598c961
thando <tzulu49@gmail.com>
2023-11-17 07:38:34 +0200

    Removed unused imports in Investor, Product and Address entities

--- src/main/java/com/example/investmentsapi/model/Product.java
+++ src/main/java/com/example/investmentsapi/model/Product.java
@@ -1,9 +1,7 @@
 package com.example.investmentsapi.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
